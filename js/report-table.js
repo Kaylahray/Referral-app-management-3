a4a9d741008ba1b75314e6b035ae3bf3a00b48bc
gabito1451 <obiagaziegabriel@gmail.com>
2024-08-04 18:43:45 +0200

    Added functionality to Button element in report-table.html. Made create-prospect-uplod.html responsive

---
+++ js/report-table.js
@@ -0,0 +1,15 @@
+const createREPORT = document.querySelector(".report-table-header-btn");
+  const reportPopUp = document.querySelector(".reportoverlaycontent");
+  const cancelReport = document.querySelector(".cancel")
+  const createReportPopUp = document.querySelector(".create-report");
+  const successPopUp = document.querySelector(".successpopupcontainer")
+
+  createREPORT.onclick = function() {
+    reportPopUp.style.visibility ="visible"
+  }
+  cancelReport.onclick = function(){
+    reportPopUp.style.visibility ="hidden"
+  }
+  createReportPopUp.onclick = function () {
+  successPopUp.style.visibility = "visible"
+  }
