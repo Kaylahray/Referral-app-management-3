a77657532feafbe2ea85aba1e63d6075d8ec941f
Benard Simon <benardsimon7@gmail.com>
2024-08-06 09:28:09 +0100

    Fixed the button tag style affecting general code and also updated the settings js.

--- js/settings.js
+++ js/settings.js
@@ -1,31 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const settingsOptions = document.querySelectorAll(".settings-option");
-    const contentSections = document.querySelectorAll(".content-section");
+if (typeof loadSettings === "undefined") {
+    const loadSettings = () => {
+      const settingsOptions = document.querySelectorAll(
+        ".settings-option"
+      );
+      const contentSections = document.querySelectorAll(
+        ".content-section"
+      );
   
-    settingsOptions.forEach((option) => {
-      option.addEventListener("click", function () {
-        console.log("Option clicked:", this.id);
+      settingsOptions.forEach((option) => {
+        option.addEventListener("click", function () {
+          console.log("Option clicked:", this.id);
   
-        // Remove 'active' class from all options
-        settingsOptions.forEach((opt) => opt.classList.remove("active"));
+          // Remove 'active' class from all options
+          settingsOptions.forEach((opt) =>
+            opt.classList.remove("active")
+          );
   
-        // Hide all content sections
-        contentSections.forEach((section) => (section.style.display = "none"));
+          // Hide all content sections
+          contentSections.forEach(
+            (section) => (section.style.display = "none")
+          );
   
-        // Add 'active' class to the clicked option
-        this.classList.add("active");
+          // Add 'active' class to the clicked option
+          this.classList.add("active");
   
-        // Display the corresponding content section
-        const contentClass = `${this.id}-content`;
-        const contentSection = document.querySelector(`.${contentClass}`);
-        console.log("Content section:", contentSection);
+          // Display the corresponding content section
+          const contentClass = `${this.id}-content`;
+          const contentSection = document.querySelector(
+            `.${contentClass}`
+          );
+          console.log("Content section:", contentSection);
   
-        if (contentSection) {
-          contentSection.style.display = "block";
-        } else {
-          console.error(`Content section with class ${contentClass} not found.`);
-        }
+          if (contentSection) {
+            contentSection.style.display = "block";
+          } else {
+            console.error(
+              `Content section with class ${contentClass} not found.`
+            );
+          }
+        });
       });
-    });
-  });
-  
+    };
+    loadSettings();
+  } else {
+    loadSettings();
+  }
