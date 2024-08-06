cbbf3709d9bf67fdad882e69ce9324c8212fc25e
chioma <chiomachristopher91@gmail.com>
2024-08-06 04:18:11 +0100

    report page fix. dashboard page added

--- js/report.js
+++ js/report.js
@@ -1,13 +1,25 @@
 if (typeof loadReport === "undefined") {
   const loadReport = () => {
     const progressBars = document.querySelectorAll(
-      ".report-progress-bar .report-progress"
+      ".report-progress"
     );
-    console.log(progressBars);
+    const percentageTexts = document.querySelectorAll(
+      ".report-percentage"
+    );
+
+    // Loop through each percentageText to set its color based on the data-color attribute
+    percentageTexts.forEach((text) => {
+      const color = text.getAttribute("data-color");
+      if (color) {
+        text.style.color = color;
+        text.style.borderColor = color;
+      }
+    });
+
+    // Loop through each progressBar to set its width and background color based on the data-percentage and data-color attributes
     progressBars.forEach((bar) => {
-      const percentageText = bar
-        .closest("tr")
-        .querySelector(".report-percentage");
+      const percentageText = bar.querySelector(".report-percentage");
+      const color = bar.getAttribute("data-color");
       let percentage = parseInt(
         bar.getAttribute("data-percentage"),
         10
@@ -20,8 +32,13 @@ if (typeof loadReport === "undefined") {
         } else {
           width++;
           bar.style.width = width + "%";
+
           if (percentageText) {
             percentageText.textContent = width + "%";
+          }
+
+          if (color) {
+            bar.style.backgroundColor = color;
           }
         }
       }, 20); // Speed of the animation
