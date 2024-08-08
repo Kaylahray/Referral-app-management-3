0196eb151c3d50140a306674850f43d4c6cf2a75
gabito1451 <obiagaziegabriel@gmail.com>
2024-08-08 10:53:34 +0200

    patch

--- js/create-prospect-upload.js
+++ js/create-prospect-upload.js
@@ -1,17 +1,24 @@
-if (typeof loadProspectUpload === "undefined") {
-  const loadProspectUpload = () => {
-    const createProspectBtn = document.getElementById(
-      "createProspectBtn"
-    );
-    const successPopUpModal = document.querySelector(
-      ".successpopupmodalcontainer"
-    );
-
-    createProspectBtn.onclick = function () {
-      successPopUpModal.style.visibility = "visible";
-    };
-  };
-  loadProspectUpload();
-} else {
-  loadProspectUpload();
+const yesBttn = document.querySelector(".yesbtn");
+constsuccessPop = document.querySelector(".successpopupmodalcontainer");
+yesBttn.onclick = function() {
+  constsuccessPop.style.display = "flex"
 }
+
+
+// if (typeof loadProspectUpload === "undefined") {
+//   const loadProspectUpload = () => {
+//     const createProspectBtn = document.getElementById(
+//       "createProspectBtn"
+//     );
+//     const successPopUpModal = document.querySelector(
+//       ".successpopupmodalcontainer"
+//     );
+
+//     createProspectBtn.onclick = function () {
+//       successPopUpModal.style.visibility = "visible";
+//     };
+//   };
+//   loadProspectUpload();
+// } else {
+//   loadProspectUpload();
+// }
