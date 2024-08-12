cdce2ae0995b8d1fe69306fe233b16c1314b9a45
gabito1451 <obiagaziegabriel@gmail.com>
2024-08-13 01:00:53 +0200

    fix

--- js/report-table.js
+++ js/report-table.js
@@ -3,6 +3,32 @@
     const cancelReport = document.querySelector(".cancel");
     const createReportPopUp = document.querySelector(".create-report");
     const successPopUp = document.querySelector(".successpopupcontainer");
+    const currentReportingOption  = document.querySelector(".currentReportingOption");
+    const checkBoxInput = document.querySelector (".checkBoxInput");
+    const scrollableContainer = document.querySelector(".scrollable-container");
+    const reportSelectLinks = document.querySelector(".reportselect-links");
+    const popUp = document.querySelector(".reportpop-up")
+ 
+    //  checked box funtion
+
+    checkBoxInput.addEventListener("change", () => {
+      if (checkBoxInput.checked) {
+        scrollableContainer.style.display = "none";
+        reportSelectLinks.style.display = "none";
+        currentReportingOption.style.display = "block";
+      } else {
+        scrollableContainer.style.display = "block";
+        currentReportingOption.style.display = "none";
+        reportSelectLinks.style.display = "flex"
+      }
+    });
+
+    reportPopUp.addEventListener("click", () => {
+      reportPopUp.style.visibility = "hidden"
+    })
+         
+    
+
 
     // Show the report popup
     createREPORT.onclick = function () {
@@ -19,6 +45,11 @@
     createReportPopUp.onclick = function () {
       successPopUp.style.visibility = "visible";
     };
+
+    popUp.addEventListener("click", (e) =>{
+      e.stopPropagation();
+      return;
+    })
 
 
 
